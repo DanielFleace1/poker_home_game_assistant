6fb267afc7889a1d12a786eef04e546d4f7ea1a2
Daniel Fleace <danielfleace15@gmail.com>
2022-05-04 14:51:21 -0400

    Add form with inputs that can edit players buy ins & end chip count & correctly update state

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -5,16 +5,62 @@ import '../App.css'
 
 function Game() {
     // State
-    const [buyInCost, , chipsPerBuyIn] = useOutletContext()
+    const [buyInCost, , chipsPerBuyIn, , playersArray, setPlayersArray] = useOutletContext()
+    const [newPlayer, setNewPlayer] = useState('')
     const [alertMsg, setAlertMsg] = useState(null)
     const [alertSeverity, setAlertSeverity] = useState(null)
-
+    const [indexNum, setIndexNum] = useState(0)
     const navigate = useNavigate()
+
     // Handlers
     const handleExit = () => {
         navigate('/')
     }
-
+    const addPlayer = (e) => {
+        e.preventDefault()
+        const obj = {
+            name: newPlayer,
+            buyIns: 0,
+            endingChips: 0,
+            index: indexNum,
+        }
+        setIndexNum(indexNum + 1)
+        setPlayersArray([...playersArray, obj])
+        setNewPlayer('')
+    }
+    const handleSetNewPlayer = (e) => {
+        setNewPlayer(e.target.value)
+    }
+    const handleCalculatePayouts = (e) => {
+        e.preventDefault()
+    }
+    const handleBuyInChange = (e) => {
+        const { name, value } = e.target
+        const playerIndex = playersArray.findIndex((x) => x.index === Number(name))
+        if (playerIndex === -1) {
+            // to do: handle error gracefully
+        } else {
+            setPlayersArray([
+                ...playersArray.slice(0, playerIndex),
+                { ...playersArray[playerIndex], buyIns: value },
+                ...playersArray.slice(playerIndex + 1),
+            ])
+        }
+    }
+    const handleEndChipCountChange = (e) => {
+        const { name, value } = e.target
+        const playerIndex = playersArray.findIndex((x) => x.index === Number(name))
+        if (playerIndex === -1) {
+            // to do: handle error gracefully
+        } else {
+            setPlayersArray([
+                ...playersArray.slice(0, playerIndex),
+                { ...playersArray[playerIndex], endingChips: value },
+                ...playersArray.slice(playerIndex + 1),
+            ])
+        }
+    }
+    // Effects
     useEffect(() => {
         if (Number(buyInCost) === 0 || Number(chipsPerBuyIn) === 0) {
             setAlertMsg(
@@ -26,7 +72,7 @@ function Game() {
                 navigate('/')
                 setAlertMsg('')
                 setAlertSeverity('')
-            }, 5000)
+            }, 1000)
         }
     }, [buyInCost, chipsPerBuyIn, navigate])
 
@@ -37,6 +83,42 @@ function Game() {
             <div> Buy in Cost: {buyInCost || '0'}</div>
             <div> Chips per Buy In: {chipsPerBuyIn || '0'}</div>
             <div> $/Chip {buyInCost / chipsPerBuyIn || '0'}</div>
+            <br />
+            <form onSubmit={addPlayer}>
+                add Player
+                <input
+                    value={newPlayer}
+                    onChange={handleSetNewPlayer}
+                    placeholder="Enter player name"
+                />
+                <button type="submit">addPlayer</button>
+            </form>
+            <br />
+            <br />
+            <div style={{ display: 'flex' }}>
+                <div>player Name </div> <div> ::Buy ins</div>
+            </div>
+            <form onSubmit={handleCalculatePayouts}>
+                {playersArray.map((player) => {
+                    return (
+                        <div key={player.index}>
+                            {player.name}{' '}
+                            <input
+                                onChange={handleBuyInChange}
+                                name={player.index}
+                                value={player.buyIns}
+                            />{' '}
+                            <input
+                                onChange={handleEndChipCountChange}
+                                name={player.index}
+                                value={player.endingChips}
+                            />
+                        </div>
+                    )
+                })}
+                <br />
+                <button type="submit">calculate payouts</button>
+            </form>
             <button type="button" onClick={handleExit}>
                 Exit Game - Home
             </button>
@@ -45,3 +127,18 @@ function Game() {
 }
 
 export default Game
+
+// const handleRemovePlayer = (e) => {
+//     const { name } = e.target
+//     const playerIndex = playersArray.findIndex((x) => x.index === Number(name))
+//     if (playerIndex === -1) {
+//         // to do: handle error gracefully
+//     } else {
+//         setPlayersArray([
+//             ...playersArray.slice(0, playerIndex),
+//             ...playersArray.slice(playerIndex + 1),
+//         ])
+//     }
+// }
+
+/* <button type="button" onClick={handleRemovePlayer}> Remove Player </button> */
